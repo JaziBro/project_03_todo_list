faa4d1ad78c722569c0d9965be5cfcd461bf016b
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-09-21 10:01:09 -0700

    .

--- main.js
+++ main.js
@@ -2,9 +2,6 @@
 import inquirer from "inquirer";
 let todos = [];
 let loop = true;
-let answers1;
-let answers2;
-let answers3;
 async function startLoop() {
     while (loop) {
         await displayMenuItems();
@@ -12,7 +9,7 @@ async function startLoop() {
 }
 startLoop();
 async function displayMenuItems() {
-    answers1 = await inquirer.prompt([
+    let answer1 = await inquirer.prompt([
         {
             type: "input",
             name: "MenuOpt",
@@ -20,7 +17,7 @@ async function displayMenuItems() {
             message: "Please select your menu item"
         }
     ]);
-    switch (answers1.menuOpt) {
+    switch (answer1.MenuOpt) {
         case `AddTodoItem`: {
             await addTodo();
             break;
@@ -39,13 +36,13 @@ async function displayMenuItems() {
     }
 }
 async function addTodo() {
-    answers2 = await inquirer.prompt([
+    let answer2 = await inquirer.prompt([
         {
             type: "input",
             name: "Todo",
             message: "Enter What To Do"
         }
     ]);
-    todos.push(answers2.todo);
+    todos.push(answer2.Todo);
     console.log(todos);
 }
--- main.ts
+++ main.ts
@@ -1,16 +1,8 @@
 #! usr/bin/env node
 import inquirer from "inquirer"
 
-interface ansType {
-    menuOpt: string,
-    todo: string
-}
-
 let todos: [] = []
 let loop: boolean = true
-let answers1: ansType
-let answers2: ansType
-let answers3: ansType
 
  async function startLoop (){
     while (loop) {
@@ -20,7 +12,7 @@ let answers3: ansType
 startLoop()
 
 async function displayMenuItems() {
-    answers1 = await inquirer.prompt ([
+   let answer1 = await inquirer.prompt ([
         {
             type: "input",
             name: "MenuOpt",
@@ -28,7 +20,7 @@ async function displayMenuItems() {
             message: "Please select your menu item"
         }
 ]);
-switch(answers1.menuOpt){
+switch(answer1.MenuOpt){
     case `AddTodoItem`: {
         await addTodo()
         break
@@ -48,14 +40,14 @@ switch(answers1.menuOpt){
 }
 
 async function addTodo(){
-    answers2 = await inquirer.prompt([
+    let answer2 = await inquirer.prompt([
         {
             type: "input",
             name: "Todo",
             message: "Enter What To Do"
         }
     ])
-    todos.push(answers2.todo)
+    todos.push(answer2.Todo)
     console.log(todos)
 }
 
