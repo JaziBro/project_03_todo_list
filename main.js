0d393e41c343c963e922c08ff5f5f3279862c1ae
jazilhashmi <138767638+JaziBro@users.noreply.github.com>
2023-09-21 08:26:14 -0700

    .

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+#! usr/bin/env node
+let todos = [];
+let loop = true;
+let ans1;
+let ans2;
+let ans3;
 export {};
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+#! usr/bin/env node
 import inquirer from "inquirer"
 
+let todos: [] = []
+let loop: boolean = true
+let ans1: string 
+let ans2: string 
+let ans3: string 
+
+function 
 
